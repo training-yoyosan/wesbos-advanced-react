75a9d18dffd20c4d1488dfd067c23fdeaf738887
YoloSan <veselu@gmail.com>
2021-05-27 22:12:08 +0300

    add progress when changing routes

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,10 +1,10 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ro-RO">
-        {/*<Head></Head>*/}
+        <Head />
 
         <body>
           <Main />
